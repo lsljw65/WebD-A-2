120e2e0f17191d4ce3437fa319d26cd642fe05c9
lee <lsljw65@gmail.com>
2024-10-24 09:46:33 +0900

    콘텐츠 슬라이드 구현

--- js/script.js
+++ js/script.js
@@ -17,4 +17,17 @@ $(function () {
     $(".sub-back").stop().slideUp();
     $(".sub-menu").stop().slideUp();
   });
+  //   슬라이드
+  var count = 0;
+  setInterval(function () {
+    if (count === 3) {
+      count = 0;
+    }
+    $(".sliders")
+      .stop()
+      .animate({
+        left: -1200 * count,
+      });
+    count++;
+  }, 3000);
 });
